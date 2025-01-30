311b4779fd87950c3aa015aea1d0528b76123ac5
hamisiyawa99 <hamisiyawa99@gmail.com>
2025-01-30 14:50:36 +0300

    second commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new PureCounter();
+});
+
+//JQUERY CODE
+(function ($) {
+    "use strict";
+  
+    // Back to top button visibility control
+    function checkScroll() {
+        if ($(window).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    }
+  
+    // Bind the scroll event to check button visibility
+    $(window).on('scroll', checkScroll);
+  
+    // Back to top button click event
+    $('.back-to-top').click(function () {
+        // Animate the scroll to the top
+        $('html, body').stop().animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+  
+        // Temporarily unbind scroll during animation and rebind afterward
+        $(window).off('scroll');
+        $(window).on('scroll', checkScroll);
+    });
+  
+  })(jQuery);
